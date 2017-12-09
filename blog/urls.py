9e1f2837ab081e96a8787ca345495a63387d7953
wangxuan <wangxuan@wangxuandeMacBook-Pro.local>
2017-12-09 09:43:40 +0800

    sold mac

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-        url(r'^$',views.home_page,name='home_page'),
+#        url(r'^$',views.home_page,name='home_page'),
         url(r'^blogs$',views.BlogList.as_view(),name='blogs'),
         url(r'^blogs/(?P<id>[0-9]+)$',views.BlogDetail.as_view(),name='blog_detail'),
         ]
